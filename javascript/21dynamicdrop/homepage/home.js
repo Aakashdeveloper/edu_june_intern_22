6173da707e06d98694afb9d46b752351d58366d4
aakashdeveloper <ahanda205@gmail.com>
2022-09-17 15:43:43 +0530

    nodejs

--- javascript/21dynamicdrop/homepage/home.js
+++ javascript/21dynamicdrop/homepage/home.js
@@ -16,6 +16,32 @@ function getCity(){
     })
 }
 
+const getCity = async() => {
+    let res = await fetch(cityUrl,{method:'GET'})
+    let data = await res.json()
+    data.map((item) => {
+        let element = document.createElement('option') //<option></option>
+        let text = document.createTextNode(item.state) // Delhi
+        element.appendChild(text) //<option>Delhi</option>
+        element.value = item.state_id //<option value="1">Delhi</option>
+        document.getElementById('city').appendChild(element) 
+        //<select><option value="1">Delhi</option></select>
+    })
+}
+
+
+async function getCity(){
+    let res = await fetch(cityUrl,{method:'GET'})
+    let data = await res.json()
+    data.map((item) => {
+        let element = document.createElement('option') //<option></option>
+        let text = document.createTextNode(item.state) // Delhi
+        element.appendChild(text) //<option>Delhi</option>
+        element.value = item.state_id //<option value="1">Delhi</option>
+        document.getElementById('city').appendChild(element) 
+        //<select><option value="1">Delhi</option></select>
+    })
+}
 
 function getRest(){
     let cityId = document.getElementById('city').value;
